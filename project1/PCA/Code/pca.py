2fe49441760b97ec4a7320aca9526bdd20e93cba
Kamran <Kamran@MacBook-Air.local>
2019-09-07 12:27:08 -0400

    Created Template for PCA in pca.py

---
+++ project1/PCA/Code/pca.py
@@ -0,0 +1,12 @@
+#Read files and get data points
+#input : .txt file with n rows and d columns
+#output : an n*d matrix
+
+def read_data():
+    #TODO
+
+#PCA Algorithm
+#input: n*d matrix
+#output: 2*2 matrix
+def pca(matrix):
+    #TODO
