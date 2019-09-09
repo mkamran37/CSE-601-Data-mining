c4907ab279c3292fd4bad737d05e79d4f183ade4
Areeb-Aatif <areebuddin95@gmail.com>
2019-09-08 23:22:06 -0400

    svd code

--- project1/PCA/Code/tSNE.py
+++ project1/PCA/Code/tSNE.py
@@ -1,14 +1,13 @@
 from sklearn.manifold import TSNE
-import numpy as np
-from pca import read_data
+from pca import read_data, scatter_plot
 
 def T_SNE(data):
     tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=500)
-    tsne_results = tsne.fit_transform(data)
-    print(tsne_results)
+    return tsne.fit_transform(data)
 
 if __name__ == "__main__":
-    data, diseases = read_data("../../Data/pca_a.txt")
+    data, diseases = read_data("CSE-601/project1/Data/pca_a.txt")
     # data, diseases = read_data("CSE-601/project1/Data/pca_b.txt")
     # data, diseases = read_data("CSE-601/project1/Data/pca_c.txt")
-    T_SNE(data)
+    tsne_result = T_SNE(data)
+    scatter_plot(tsne_result, diseases)
