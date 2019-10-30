e17f33f427008e3f0086f87a668d432679d5b59c
Areeb-Aatif <areebuddin95@gmail.com>
2019-10-30 18:54:04 -0400

    removed pycache files

--- project2/Code/Gmm.py
+++ project2/Code/Gmm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import multivariate_normal
+from scipy.stats import norm
 
 class gmm:
 
@@ -9,21 +10,23 @@ class gmm:
         self.mu = centroids
         self.pi = None
         self.sigma = None
+        self.log_likelihood = [0]
+        print("\nRequire input parameters for gaussian mixture model ....................")
         self.dataMatrix, self.geneIds = self.readData(filePath)
-        self.reg_sigma = 1e-9*np.identity(len(self.dataMatrix[0]))
         self.readParams()
 
     def readParams(self):
-        self.numClusters = int(input("enter number of clusters: "))
-        self.threshold = int(input("enter convergence threshold: "))
-        self.maxIterations = int(input("enter maximum iterations: "))
-        self.mu = [[0,0],[1,1]]
-        # self.sigma = np.zeros((self.numClusters, len(self.dataMatrix[0]), len(self.dataMatrix[0])), dtype='float')
-        self.sigma = [[[1,1],[1,1]], [[2,2], [2,2]]]
+        self.numClusters = int(input("Enter number of clusters: "))
+        self.threshold = float(input("Enter convergence threshold: "))
+        self.maxIterations = int(input("Enter maximum iterations: "))
+        self.reg_sigma = 1e-9*np.ones(len(self.dataMatrix[0]))
+        self.mu = [[0,0],[1,1],[2,2]]
+        # self.sigma = np.ones((self.numClusters, len(self.dataMatrix[0]), len(self.dataMatrix[0])), dtype='float')
+        self.sigma = [[[1,1],[1,1]], [[2,2], [2,2]], [[3,3], [3,3]]]
         # for dim in range(len(self.sigma)):
         #     np.fill_diagonal(self.sigma[dim], 1)
         # self.pi = np.ones(self.numClusters) / self.numClusters
-        self.pi = [0.5, 0.5]
+        self.pi = [0.5, 0.5, 0.5]
 
     def readData(self, filePath):
         #Read data from text file as numpy ndarray
@@ -33,10 +36,15 @@ class gmm:
         return data, geneIds
 
     def emAlgorithm(self):
+        print("\nRunning Gaussian Mixture Model ....................")
         for i in range(self.maxIterations):
             self.probMatrix = self.eStep()
-            self.log_likelihood = self.mStep()
-            print("Log Likelihood (Iteration: " + (i+1) + "): " + str(self.log_likelihood))
+            logL = self.mStep()
+            self.log_likelihood.append(logL)
+            print("Log Likelihood (Iteration: " + str(i+1) + "): " + str(logL))
+            if abs(self.log_likelihood[-1] - self.log_likelihood[-2]) < self.threshold:
+                print("Reached convergence threshold ....................")
+                break
 
         predictedMatrix = self.getClusters()
         return self.dataMatrix, predictedMatrix, self.geneIds
@@ -45,12 +53,13 @@ class gmm:
         # probMatrix (rik) = n x k where n = number of data points, k = number of clusters
         probMatrix = np.zeros((self.dataMatrix.shape[0], self.numClusters))
         for mu, sig, pi, idx in zip(self.mu, self.sigma, self.pi, range(self.numClusters)):
+                sig += self.reg_sigma
                 probMatrix[:, idx] = self.rik(mu, sig, pi)
         return probMatrix
 
     def rik(self, mu, sigma, pi):
-        numerator = pi*multivariate_normal(mean=mu, cov=sigma, allow_singular=True).pdf(self.dataMatrix)
-        denominator = np.sum([p*multivariate_normal(mean=m, cov=s, allow_singular=True).pdf(self.dataMatrix)
+        numerator = pi*multivariate_normal.pdf(self.dataMatrix, mean=mu, cov=sigma, allow_singular=True)
+        denominator = np.sum([p*multivariate_normal.pdf(self.dataMatrix, mean=m, cov=s, allow_singular=True)
                                     for p, m, s in zip(self.pi, self.mu, self.sigma+self.reg_sigma)], axis=0)
         return numerator / denominator
 
@@ -69,7 +78,7 @@ class gmm:
                             *(self.dataMatrix - new_mu)).T, (self.dataMatrix - new_mu))) / sigma_rik
             self.sigma.append(new_sigma + self.reg_sigma)
 
-        log_likelihood = np.log(np.sum([pi*multivariate_normal(mean=self.mu[m],cov=self.sigma[s], allow_singular=True).pdf(self.dataMatrix) 
+        log_likelihood = np.log(np.sum([pi*multivariate_normal.pdf(self.dataMatrix, mean=self.mu[m], cov=self.sigma[s]+self.reg_sigma, allow_singular=True) 
                                     for pi,m,s in zip(self.pi,range(len(self.mu)),range(len(self.sigma)))]))
         return log_likelihood
 
